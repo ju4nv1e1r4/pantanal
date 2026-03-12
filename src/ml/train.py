d2355da92a6ff5c1f988a2ff2a6f1570af8a3da5
Juan Vieira <juanvieira1@yahoo.com>
2026-03-12 18:13:46 -0300

    refactor: trasnform audio class now is a module
    Signed-off-by: Juan Vieira <juanvieira1@yahoo.com>

--- src/ml/train.py
+++ src/ml/train.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
-import torchaudio.transforms as T
+import torchaudio.transforms as Tn
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
@@ -15,30 +15,8 @@ from torch.amp import autocast, GradScaler
 from src.data.data_loader import DeepWetlandsDataset
 from src.ml.efficientnet_b0 import DeepWetlandsModel
 from src.ml.training_logger import TrainingLogger
+from src.ml.audio_transform import GPUAudioTransform
 
-
-class GPUAudioTransform(nn.Module):
-    def __init__(self, target_sample_rate=32000):
-        super().__init__()
-        self.mel_spectrogram = T.MelSpectrogram(
-            sample_rate=target_sample_rate,
-            n_fft=2048,
-            hop_length=512,
-            n_mels=128,
-            f_min=0,
-            f_max=16000
-        )
-        self.db_transform = T.AmplitudeToDB()
-        self.time_mask = T.TimeMasking(time_mask_param=30)
-        self.freq_mask = T.FrequencyMasking(freq_mask_param=15)
-
-    def forward(self, waveform, is_train=False):
-        spec = self.mel_spectrogram(waveform)
-        spec = self.db_transform(spec)
-        if is_train:
-            spec = self.freq_mask(spec)
-            spec = self.time_mask(spec)
-        return spec
 
 def mixup_data(x, y, alpha=0.5):
     if alpha > 0:
